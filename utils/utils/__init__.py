856f0cc25130d958924c0aea8cd7a38e79f00663
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-10-13 15:38:33 +0200

    cleanup

--- utils/utils/__init__.py
+++ utils/utils/__init__.py
@@ -5,10 +5,12 @@ from . import folders
 from . import plot
 from .io import read_ita
 from .edc import truncated_schroeder_integration
+from .plot import attach_fractional_octave_ticker
 
 __all__ = [
     'folders',
     'plot',
     'read_ita',
-    'truncated_schroeder_integration'
+    'truncated_schroeder_integration',
+    'attach_fractional_octave_ticker'
 ]
